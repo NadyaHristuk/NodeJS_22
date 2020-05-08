c5baa7723a4f364de061f4f1ac4a6efc48fedd31
Nadin <elhe@i.ua>
2020-05-08 18:56:14 +0300

    add

---
+++ 9_PostgreSQL/postgres_express_node/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const logger = require('morgan');
+
+const app = express();
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+require('./server/routes')(app);
+app.get('*', (req, res) =>
+	res.status(200).send({
+	                                                                  message: 'Welcome to the beginning of nothingness.',
+	})
+);
+
+
+const port = parseInt(process.env.PORT, 10) || 8000;
+app.set('port', port);
+
+
+app.listen(port, () => {
+  console.log(`The server is running at localhost:${port}`);
+});
+
